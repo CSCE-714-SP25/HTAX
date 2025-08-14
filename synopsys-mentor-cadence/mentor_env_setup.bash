e7149a6b63e2851b633f3644a5936477d15be60f
Houngninou <davidkebo@D3Q5J9D2-221765.local>
2025-08-14 15:08:41 -0500

    Tools setup

---
+++ synopsys-mentor-cadence/mentor_env_setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MODELSIM_HOME=/opt/coe/mentorgraphics/modelsim-2021.1/
+
+export UVMHOME=/opt/coe/mentorgraphics/modelsim-2021.1/modeltech/uvm-1.1d/
+
+source /opt/coe/mentorgraphics/modelsim-2021.1/setup.modelsim.bash
+export PATH
+
+echo "Done Sourcing!"
---
+++ synopsys-mentor-cadence/setupX.csh
@@ -0,0 +1,6 @@
+#!/bin/csh -f
+setenv UVMHOME "/opt/coe/cadence/XCELIUM/tools/methodology/UVM/CDNS-1.1d/sv"
+source /opt/coe/cadence/XCELIUM/setup.XCELIUM.linux
+source /opt/coe/cadence/VMANAGER/setup.VMANAGER.linux
+alias imc="/opt/coe/cadence/VMANAGER/bin/imc"
+echo Success
